8a0c832c305d389a578c09cefde803b77472fcfb
Albert <ignatius1010@gmail.com>
2023-03-03 13:47:33 +0700

    implementing expenses chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState("2022");
@@ -23,7 +24,8 @@ function Expenses(props) {
           selectedYear={selectedYear}
           onSelectedYear={selectedYearHandler}
         />
-        <ExpensesList items={filteredExpenses}/>            
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />            
       </Card>
     </div>
   );
